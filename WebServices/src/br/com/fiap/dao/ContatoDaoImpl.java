3effcab9ec3b9883f202dbb92aa8c2bfa8a0445f
Fernando Santiago <fspoliveira@yahoo.com.br>
2012-05-23 20:20:24 -0300

    Get and Save

--- WebServices/src/br/com/fiap/dao/ContatoDaoImpl.java
+++ WebServices/src/br/com/fiap/dao/ContatoDaoImpl.java
@@ -8,62 +8,49 @@ import br.com.fiap.bean.Contato;
 import br.com.fiap.kvstore.KVStore;
 
 public class ContatoDaoImpl implements ContatoDao {
-	
+
 	private static List<String> majorComponents = new ArrayList<String>();
 	private static List<String> minorComponents = new ArrayList<String>();
 	private static KVStore kvstore = new KVStore();
 	Key myKey = null;
-	
+
 	@Override
 	public String save(Contato contato) {
-		
+
 		majorComponents.add(contato.getEmail());
 		minorComponents.add("nome");
 		myKey = Key.createKey(majorComponents, minorComponents);
 		return kvstore.put(myKey, contato.getNome());
-		
+
 	}
 
 	@Override
 	public String remove(Contato contato) {
 		// TODO Auto-generated method stub
 		return "Removido com sicesso";
-		
+
 	}
 
 	@Override
 	public Contato getContato(String email) {
 		Contato c = new Contato();
-		
-		if(myKey  instanceof Key){
-myKey = Key.createKey(majorComponents, minorComponents);
-			
-			System.out.println("Email que chegou no get" + email);
-		
-			
+
+		if (myKey instanceof Key) {
+			myKey = Key.createKey(majorComponents, minorComponents);	
 			c.setNome(kvstore.get(myKey));
-		}
-		else
-		{
+		} else {
 			majorComponents.add(email);
 			minorComponents.add("nome");
 			myKey = Key.createKey(majorComponents, minorComponents);
-			
-			System.out.println("Email que chegou no get" + email);
-		
-			
 			c.setNome(kvstore.get(myKey));
-			
 		}
-		
-		
+
 		return c;
 	}
 
-	/*@Override
-	public List<Contato> list() {
-		// TODO Auto-generated method stub
-		return null;
-	}*/
+	/*
+	 * @Override public List<Contato> list() { // TODO Auto-generated method
+	 * stub return null; }
+	 */
 
 }
